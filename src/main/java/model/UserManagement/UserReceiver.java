f09901037bc6b62672a2689482d16d7f266a9cb2
PeterGeol <pietro.fasolino2003@gmail.com>
2025-01-06 18:47:34 +0100

    Specializzazioni utente per realizzazione chat

---
+++ src/main/java/model/UserManagement/UserReceiver.java
@@ -0,0 +1,47 @@
+package model.UserManagement;
+
+import enumerativeTypes.Ruolo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserReceiver extends Utente{
+    @Id @GeneratedValue
+    private int id;
+    private int unreadMessagesCount;      // Numero di messaggi non letti
+    private String lastReceivedMessage;   // Ultimo messaggio ricevuto
+
+    public UserReceiver() {
+
+    }
+    public UserReceiver(String nome, String cognome, String email, String username, String password, Ruolo ruolo) {
+        super(nome,cognome,email,username,password,ruolo);
+        this.unreadMessagesCount = 0;
+        this.lastReceivedMessage = null;
+    }
+
+
+
+    public int getUnreadMessagesCount() {
+        return unreadMessagesCount;
+    }
+
+    public void incrementUnreadMessages() {
+        this.unreadMessagesCount++;
+    }
+
+    public void resetUnreadMessages() {
+        this.unreadMessagesCount = 0;
+    }
+
+    public String getLastReceivedMessage() {
+        return lastReceivedMessage;
+    }
+
+    public void setLastReceivedMessage(String lastReceivedMessage) {
+        this.lastReceivedMessage = lastReceivedMessage;
+    }
+
+
+}
